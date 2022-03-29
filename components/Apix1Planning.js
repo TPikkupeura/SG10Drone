0076b8f42e7669439462caac2e6ee2b2fa0ebc8b
Jakub Fábik <jfabik@student.umb.sk>
2022-03-29 14:23:25 +0300

    Planning mission: fetch data from test json file, in json structure added atribute about input type

--- components/Apix1Planning.js
+++ components/Apix1Planning.js
@@ -1,12 +1,43 @@
 import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import React, {useState} from 'react'
+import * as appendix1 from "../src/appendix1.json"
+
+var title2 = 4; // original needs to be title
 
 export default function Apix1Planning({route, navigation}) {
+  const header = Object.keys(appendix1); //this will get all headers from json
   const { title } = route.params;
+  //console.log(Object.keys(appendix1.InitPlanCheck));
+  const getInputType = (jsonRow) =>{
+    var inputType = Object.keys(jsonRow);
+    return inputType[0];
+  }
+
+  function getData(datas) {
+    const keys = Object.keys(datas); // Get all keys from dictionary
+    var inputType;
+    return keys.map((key) => {
+      inputType = getInputType(datas[key])
+      return (
+        <Text key={key}>{key} : {datas[key][inputType]}</Text>
+      )
+    })
+}
+
+  function modifyData(datas, atribute){
+    var inputType = getInputType(datas[atribute]);
+    datas[atribute][inputType] = "Travel to moon twice";
+  }
+  modifyData(appendix1.InitPlanCheck, "Mission purpose");
+
   return (
     <View>
-      <Text>Appendix1</Text>
-      <Text>{title}</Text>
+      <Text>MISSION PLANNING</Text>
+
+      {/* <Text>{title}</Text> */}
+      {/* title is header from navigation */}
+      <Text>{header[title2]}</Text>
+      {getData(appendix1[header[title2]])}
     </View>
   )
 }
