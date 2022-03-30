89bff40dc734cac8237e45a30107a4ff13ae722a
Mikael Niemi <n0nimi00@students.oamk.fi>
2022-03-30 22:25:19 +0300

    Dropdown

--- components/Apix2Flight.js
+++ components/Apix2Flight.js
@@ -1,7 +1,10 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
+import appendix2 from '../src/appendix2.json'
 
 export default function Apix2Flight({navigation}) {
+  const header = Object.keys(appendix2);
+  
   return (
     <View>
       <Text>Appendix2</Text>
