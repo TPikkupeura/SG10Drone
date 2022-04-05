40ea89db9146a7f402ac1d01e02767ec37f88dbc
Mikael Niemi <n0nimi00@students.oamk.fi>
2022-04-06 00:30:13 +0300

    Mission.js added Creating titles

--- components/Mission.js
+++ components/Mission.js
@@ -1,30 +1,54 @@
-import { StyleSheet, Text, View, Button } from 'react-native'
+import { StyleSheet, Text, View, Button, TextInput } from 'react-native'
 import React, {useState} from 'react'
 
 export default function Mission({navigation}) {
 
+    const [title, onChangeTitle] = useState("");
     const [row, setRow] = useState([]);
     const arr = [...row];
 
     function CreateMission() {
             arr.push(
-                <Button 
-                title="Mission"
+                <Button
+                key={title}
+                title={title}
                 onPress={()=> navigation.navigate('FrontPage')}>
                 </Button>
             );
             setRow(arr);
-            console.log(row);
+            //console.log(row);
     }
   
   return (
-    <View>
+    <View style={styles.container}>
+      <TextInput
+        style={styles.input}
+        onChangeText={onChangeTitle}
+        value={title}
+        placeholder="Create Title"
+      />
       <Button
       title='Create Mission'
-      onPress={()=> CreateMission()}></Button>
-        <View>{row}</View>
+      onPress={()=> CreateMission()}
+      />
+      <View style={styles.missions}>{row}</View>
     </View>
   )
 }
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  container: {
+    flex: 1
+  },
+  input: {
+    height: 40,
+    margin: 12,
+    borderWidth: 1,
+    padding: 10,
+    width: 200,
+    alignSelf: 'center'
+  },
+  missions: {
+    marginTop: 10,
+  }
+})
