98e25bcecea6c654e4a0eb7931d5f34ab32c0f14
Mikael Niemi <n0nimi00@students.oamk.fi>
2022-05-04 18:22:54 +0300

    Em Changes

--- components/LowerEmTitles.js
+++ components/LowerEmTitles.js
@@ -18,20 +18,7 @@ export default function EmTitles({route}) {
   }, []);
 
   const EmItem = ({emItem: {sentence: title, predefined}, id}) => {
-      
-    const rendEm = () => {
-      let rendArr = [];
-      for(let i = 0; i < emObject.predefined.length; i++){
-          rendArr.push(<Text
-                          key={uuid.v4()}
-                          style={[styles.text,{backgroundColor: "lightblue"}]}
-                          >
-                      {emObject.predefined[i]}
-                      </Text>);
-      }
-      return rendArr;
-  }
-    
+        
   console.log(id);
    return (
        <View style={styles.container}>
@@ -41,7 +28,7 @@ export default function EmTitles({route}) {
              </View>
            </Pressable>
            {show && (
-               <Text>{predefined}</Text>
+                <Text>{predefined}</Text>
            )}
        </View>
    );
